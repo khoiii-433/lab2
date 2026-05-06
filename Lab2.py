955b8332a73665a1728eaab3db4a1b18eb180d4b
Khoi <nguyendmkhoi77.25@ichat.sp.edu.sg>
2026-05-06 22:47:10 +0800

    lab 2

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,48 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Pytho")
+
+print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+def get_user_input():
+    user_input = input()
+    
+    number_strings = user_input.split(",")
+
+    numbers = []
+    for num in number_strings:
+        numbers.append(float(num.strip()))
+
+    return numbers
+
+def calc_average_temperature(temperature_list):
+    total = 0
+
+    for temp in temperature_list:
+        total += temp
+
+    average = total / len(temperature_list)
+    return average
+
+def calc_min_max_temperature(temperature_list):
+    min_temp = min(temperature_list)
+    max_temp = max(temperature_list)
+
+    return [min_temp, max_temp]
+
+def sort_temperature(temperature_list):
+    sorted_list = sorted(temperature_list)
+    return sorted_list
+
+def calc_median_temperature(temperature_list):
+    
+display_main_menu()
+
+temperatures = get_user_input()
+
+average = calc_average_temperature(temperatures)
+min_max = calc_min_max_temperature(temperatures)
+sorted_temps = sort_temperature(temperatures)
+
+print("Average:", average)
+print("Min and Max:", min_max)
+print("Sorted list:", sorted_temps)
---
+++ bmi.py
@@ -0,0 +1,22 @@
+def calculate_bmi(height, weight):
+    bmi = weight / (height ** 2)
+    return bmi
+
+def classify_bmi(bmi):
+    if bmi < 18.5:
+        return "Under Weight"
+    elif 18.5 <= bmi <= 25.0:
+        return "Normal Weight"
+    else:
+        return "Over Weight"
+
+height = float(input("Enter your height: "))
+weight = float(input("Enter your weight: "))
+
+bmi = calculate_bmi(height, weight)
+
+
+category = classify_bmi(bmi)
+
+print(f"Your BMI is: {bmi}")
+print(f"Weight Classification: {category}")
